41e76a36a95b6be03adbe79625588674e33da27f
Seung-Yoon Oh <feynubrick@gmail.com>
2019-03-14 13:26:38 +0900

    changed cors policy

--- app.js
+++ app.js
@@ -48,9 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
  */
 app.use(cors({
-  origin:['*'],
   methods:['GET','POST'],
-  credentials: true
 }));
 
 /**
